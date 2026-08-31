fac57b4458513ba854ba652554a4bc8f6b602378
scheilaswink <scheilaswink@gmail.com>
2026-08-31 18:36:49 -0300

    add

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = ' https://cnt-97c70dad-8862-4235-815d-410507862d2f.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
