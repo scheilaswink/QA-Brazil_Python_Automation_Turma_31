2ee231a75c45a40ef5b90db446494f6d61e6231d
scheilaswink <scheilaswink@gmail.com>
2026-09-16 18:23:20 -0300

    test_set_route automate

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL = ' https://cnt-97c70dad-8862-4235-815d-410507862d2f.containerhub.tripleten-services.com?lng=pt'
+URBAN_ROUTES_URL = ' https://cnt-e5187916-eb9c-4f70-a838-d9a97eb1bef8.containerhub.tripleten-services.com?lng=pt'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
--- main.py
+++ main.py
@@ -1,49 +1,88 @@
+import time
+
 import data
 import helpers
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import pages
+from pages import UrbanRoutesPage
 
 
 class TestUrbanRoutes:
     @classmethod
     def setup_class(cls):
+        from selenium.webdriver import DesiredCapabilities
+        capabilities = DesiredCapabilities.CHROME
+        capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
+        cls.driver = Chrome()
+        cls.driver.implicitly_wait(5)
         if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
             print("Conectado ao servidor Urban Routes")
         else:
             print('Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.')
+
+    def  setup_method(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
+
+        self.page = UrbanRoutesPage(self.driver)
+
+    def _start_confort_flow(self):
+        self.page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+
     def test_set_route(self):
-        # Adicionar em S8
-        print("função criada para definir a rota")
-        pass
+       self.page.enter_location(data.ADDRESS_FROM, data.ADDRESS_TO)
+
+       assert UrbanRoutesPage.get_from_location(self.page) == data.ADDRESS_FROM
+       assert UrbanRoutesPage.get_to_location (self.page) == data.ADDRESS_TO
+
+       #time.sleep(10)
+
     def test_select_plan(self):
         # Adicionar em S8
         print("função criada para definir a rota")
         pass
+
     def test_fill_phone_number(self):
         # Adicionar em S8
         print("função criada para definir a rota")
         pass
+
     def test_fill_card(self):
         # Adicionar em S8
         print("função criada para definir a rota")
         pass
+
     def test_comment_for_driver(self):
         # Adicionar em S8
         print("função criada para definir a rota")
         pass
+
     def test_order_blanket_and_handkerchiefs(self):
         # Adicionar em S8
         print("função criada para definir a rota")
         pass
+
     def test_order_2_ice_creams(self):
         # Adicionar em S8
         print("função criada para definir a rota")
         pass
+
     def test_car_search_model_appears(self):
         # Adicionar em S8
         print("função criada para definir a rota")
         pass
+
     def test_order_2_ice_creams(self):
         number_of_ice_creams = 2
         for count in range(number_of_ice_creams):
             # Adicionar em S8
             print("função criada para adicionar a quantidade de sorvetes")
             pass
+
+        @classmethod
+        def teardown_class(cls):
+            cls.driver.quit()
